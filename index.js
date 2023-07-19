8ba21c7265e829068342dce691368d22ca1585ec
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2023-07-19 09:30:11 +0300

    Refactor to reduce code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const Spawn = require("child_process").spawn;
 const FS = require("fs");
 
+const configPath = "./config.json";
+
 const detached = false;
 const allowedCommands = [
   "install",
@@ -13,9 +15,17 @@ const allowedCommands = [
   "use",
 ];
 
+const allowedPms = ["npm", "yarn", "pnpm"];
+
 const npmPath = "./package-lock.json";
 const yarnPath = "./yarn.lock";
 const pnpmPath = "./pnpm-lock.yaml";
+
+const lockFileMapping = {
+  npm: { path: npmPath },
+  yarn: { path: yarnPath },
+  pnpm: { path: pnpmPath },
+};
 
 const commandsMatrix = {
   npm: {
@@ -71,96 +81,71 @@ args.shift();
 lookForLockFile();
 
 function lookForLockFile() {
-  if (allowedCommands.includes(mainArg)) {
-    if (mainArg === "use") {
-      // write to config.json the default package manager if arg[1] is valid
-      // using fs
-      if (args[0] === "npm" || args[0] === "yarn" || args[0] === "pnpm") {
-        FS.writeFileSync(
-          "./config.json",
-          JSON.stringify({ defaultPackageManager: args[0] })
-        );
-        console.log(`Setting ${args[0]} as default package manager`);
-      } else {
-        console.log(
-          `Invalid package manager, please choose between npm, yarn or pnpm`
-        );
-      }
+  if (!allowedCommands.includes(mainArg)) {
+    return;
+  }
+  if (mainArg === "use") {
+    // write to config.json the default package manager if arg[1] is valid
+    // using fs
+    if (args[0] === "npm" || args[0] === "yarn" || args[0] === "pnpm") {
+      FS.writeFileSync(
+        configPath,
+        JSON.stringify({ defaultPackageManager: args[0] })
+      );
+      console.log(`Setting ${args[0]} as default package manager`);
+    } else {
+      console.log(
+        `Invalid package manager, please choose between npm, yarn or pnpm`
+      );
+    }
+    return;
+  }
+  //check for .lock file
+  // if exists then use npm install or yarn install or pnpm install
+
+  for (const [manager, path] of Object.entries(lockFileMapping)) {
+    if (FS.existsSync(path)) {
+      generateCommand(
+        manager,
+        commandsMatrix[manager],
+        additionalArgsMatrix[manager]
+      );
       return;
     }
-    //check for .lock file
-    // if exists then use npm install or yarn install or pnpm install
-    if (
-      FS.existsSync(npmPath, FS.constants.F_OK, (err) => {
-        if (err) {
-          console.log("No package-lock.json found");
-        }
-      })
-    ) {
-      generateCommand("npm", commandsMatrix.npm, additionalArgsMatrix.npm);
-    } else if (
-      FS.existsSync(yarnPath, FS.constants.F_OK, (err) => {
-        if (err) {
-          console.log("No yarn.lock found");
-        }
-      })
-    ) {
-      generateCommand("yarn", commandsMatrix.yarn, additionalArgsMatrix.yarn);
-    } else if (
-      FS.existsSync(pnpmPath, FS.constants.F_OK, (err) => {
-        if (err) {
-          console.log("No pnpm-lock.yaml found");
-        }
-      })
-    ) {
-      generateCommand("pnpm", commandsMatrix.pnpm, additionalArgsMatrix.pnpm);
-    } else {
-      // read from config.json the default package manager
-      // if exists then use npm install or yarn install or pnpm install
+  }
 
-      if (
-        FS.existsSync("./config.json", FS.constants.F_OK, (err) => {
-          if (err) {
-            console.log("Please close config.json file and try again");
-          }
-        })
-      ) {
-        const config = JSON.parse(FS.readFileSync("./config.json"));
-        console.log(
-          "No lock file found, using default package manager " +
-            config.defaultPackageManager +
-            ". \nIf you want to change it use nodepm use <package-manager>"
-        );
-        if (config.defaultPackageManager === "npm") {
-          console.log(
-            "default package manager is npm, using the appropriate command"
-          );
-          generateCommand("npm", commandsMatrix.npm, additionalArgsMatrix.npm);
-        } else if (config.defaultPackageManager === "yarn") {
-          console.log(
-            "default package manager is yarn, using the appropriate command"
-          );
-          generateCommand(
-            "yarn",
-            commandsMatrix.yarn,
-            additionalArgsMatrix.yarn
-          );
-        } else if (config.defaultPackageManager === "pnpm") {
-          console.log(
-            "default package manager is pnpm, using the appropriate command"
-          );
-          generateCommand(
-            "pnpm",
-            commandsMatrix.pnpm,
-            additionalArgsMatrix.pnpm
-          );
-        }
-      } else {
-        console.log(
-          `No lock file found, please choose your default package manager using "nodepm use <package-manager>"  or run <package-manager> install`
-        );
+  // read from config.json the default package manager
+  // if exists then use npm install or yarn install or pnpm install
+
+  if (
+    FS.existsSync(configPath, FS.constants.F_OK, (err) => {
+      if (err) {
+        console.log("Please close config.json file and try again");
       }
+    })
+  ) {
+    const config = JSON.parse(FS.readFileSync(configPath));
+    const defaultPm = config.defaultPackageManager;
+    if (allowedPms.includes(defaultPm)) {
+      console.log(
+        "No lock file found, using default package manager " +
+          defaultPm +
+          ". \nIf you want to change it use nodepm use <package-manager>"
+      );
+      generateCommand(
+        defaultPm,
+        commandsMatrix[defaultPm],
+        additionalArgsMatrix[defaultPm]
+      );
+    } else {
+      console.log(
+        "config file corrupted, please choose your default package manager again using 'nodepm use <package-manager>'"
+      );
     }
+  } else {
+    console.log(
+      `No lock file found, please choose your default package manager using "nodepm use <package-manager>"  or run <package-manager> install`
+    );
   }
 }
 
